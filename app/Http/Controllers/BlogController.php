f6e76890441cf3b0b5684ec53c87f3fac687d3bc
penadidik <di2k.skh@gmail.com>
2019-06-26 09:21:24 +0700

    laravelapi.v1

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return blogs::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $blog = new blogs;
+        $blog->title = $request->title;
+        $blog->desBlog = $request->desBlog;
+        $blog->save();
+        
+        return "Data created";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(blog $blog)
+    {
+        //
+        return blogs::find();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, blog $blog)
+    {
+        //
+        $title = $request->title;
+        $desBlog = $request->desBlog;
+        
+        $blog = blogs::find($id);
+        $blog->title = $request->title;
+        $blog->desBlog = $request->desBlog;
+        $blog->save();
+        
+        return "Data Updated";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(blog $blog)
+    {
+        //
+        $blog = blogs::find($id);
+        $blog->delete();
+        
+        return "Data deleted";
+    }
+}
